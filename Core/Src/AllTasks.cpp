383f5e6642cf0e6825e6027489c92e24a5a51757
igorpieniek <pieniek97@gmail.com>
2020-11-13 19:27:04 +0100

    Add adc to process

--- Core/Src/AllTasks.cpp
+++ Core/Src/AllTasks.cpp
@@ -15,6 +15,9 @@ osThreadId BatteryManagerHandle;
 
 void StartBatteryManagerTask(void const * argument);
 
+uint32_t receiveADC[2];
+
+
 void AllTasks_init(){
 
 	  osThreadDef(BatteryManagerTask, StartBatteryManagerTask, osPriorityNormal, 0, 256);
@@ -26,18 +29,18 @@ void AllTasks_init(){
 
 void StartBatteryManagerTask(void const * argument){
 
-	uint32_t receiveADC[2];
+
 	uint32_t counter = 0;
 	LipoCell lipo;
-
-
+	HAL_ADC_Start(&hadc1);
+	HAL_ADC_Start_DMA(&hadc1, receiveADC, 2);
 
 	for(;;){
 
 		lipo.update(receiveADC[0]);
 		float res = lipo.getPercentage();
 		char buf[15];
-		uint16_t size = sprintf( buf, "Percent: %.2f\n", (float)res);
+		uint16_t size = sprintf( &buf[0], "Percent: %.2f\n", res);
 		HAL_UART_Transmit(&huart3, (uint8_t*)buf,size, 10);
 		osDelay(100);
 
