90af5ca977d39757975096e12e11e712bb7692e7
igorpieniek <pieniek97@gmail.com>
2020-11-14 20:56:03 +0100

    create Fun class

---
+++ Core/Inc/Fun.cpp
@@ -0,0 +1,18 @@
+/*
+ * Fun.cpp
+ *
+ *  Created on: 14.11.2020
+ *      Author: Igor
+ */
+
+#include <Fun.h>
+
+Fun::Fun() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Fun::~Fun() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Core/Inc/Fun.h
@@ -0,0 +1,17 @@
+/*
+ * Fun.h
+ *
+ *  Created on: 14.11.2020
+ *      Author: Igor
+ */
+
+#ifndef INC_FUN_H_
+#define INC_FUN_H_
+
+class Fun {
+public:
+	Fun();
+	virtual ~Fun();
+};
+
+#endif /* INC_FUN_H_ */
