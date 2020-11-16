e5480b09461f7ca080c57879bcb571c979db21f0
igorpieniek <pieniek97@gmail.com>
2020-11-16 17:36:30 +0100

    Create class files

---
+++ Core/Inc/HallSensor.h
@@ -0,0 +1,17 @@
+/*
+ * HallSensor.h
+ *
+ *  Created on: 16.11.2020
+ *      Author: Igor
+ */
+
+#ifndef INC_HALLSENSOR_H_
+#define INC_HALLSENSOR_H_
+
+class HallSensor {
+public:
+	HallSensor();
+	virtual ~HallSensor();
+};
+
+#endif /* INC_HALLSENSOR_H_ */
---
+++ Core/Src/HallSensor.cpp
@@ -0,0 +1,18 @@
+/*
+ * HallSensor.cpp
+ *
+ *  Created on: 16.11.2020
+ *      Author: Igor
+ */
+
+#include <HallSensor.h>
+
+HallSensor::HallSensor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+HallSensor::~HallSensor() {
+	// TODO Auto-generated destructor stub
+}
+
