d6bcfb79bdbb12ce80883f67a640cf05a47b2731
igorpieniek <pieniek97@gmail.com>
2020-11-17 13:47:18 +0100

    prepare definitions of functions in header

--- Core/Inc/BatteryManager.h
+++ Core/Inc/BatteryManager.h
@@ -8,10 +8,35 @@
 #ifndef INC_BATTERYMANAGER_H_
 #define INC_BATTERYMANAGER_H_
 
+#include "main.h"
+#include "LipoCell.h"
+#include "cmsis_os.h"
+
+#define LIPOCELLS_NUMBER 4				// max number of lipocell objects
+#define INDEX_OUT_OF_RANGE_VALUE 0.0f   // number to return in case user want number of cell out of index
+
+
 class BatteryManager {
 public:
+
+	void init();
+	void process();
+
+	float getBatteryVoltage();
+	float getBatteryLevel();
+	float getCellVoltage(uint8_t);
+
 	BatteryManager();
 	virtual ~BatteryManager();
+
+private:
+
+	void updateMeasurments();
+	void checkBatteryLevel();
+	void add(LipoCell);
+
+	LipoCell* cells[LIPOCELLS_NUMBER];
+
 };
 
 #endif /* INC_BATTERYMANAGER_H_ */
