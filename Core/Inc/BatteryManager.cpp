67ebb34e24c548cb3507df62bbe0b410487c0f7d
igorpieniek <pieniek97@gmail.com>
2020-11-17 13:38:45 +0100

    Add batteryManager class files

---
+++ Core/Inc/BatteryManager.cpp
@@ -0,0 +1,18 @@
+/*
+ * BatteryManager.cpp
+ *
+ *  Created on: 17.11.2020
+ *      Author: Igor
+ */
+
+#include <BatteryManager.h>
+
+BatteryManager::BatteryManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BatteryManager::~BatteryManager() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Core/Inc/BatteryManager.h
@@ -0,0 +1,17 @@
+/*
+ * BatteryManager.h
+ *
+ *  Created on: 17.11.2020
+ *      Author: Igor
+ */
+
+#ifndef INC_BATTERYMANAGER_H_
+#define INC_BATTERYMANAGER_H_
+
+class BatteryManager {
+public:
+	BatteryManager();
+	virtual ~BatteryManager();
+};
+
+#endif /* INC_BATTERYMANAGER_H_ */
