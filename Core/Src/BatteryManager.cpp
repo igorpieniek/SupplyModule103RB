21933e0fd2909c561f9daaba628e5b81c3fb0f3c
igorpieniek <pieniek97@gmail.com>
2020-11-17 13:51:27 +0100

    add empty batterymanager class functions body to .cpp

--- Core/Src/BatteryManager.cpp
+++ Core/Src/BatteryManager.cpp
@@ -7,6 +7,26 @@
 
 #include <BatteryManager.h>
 
+void BatteryManager::init(){
+
+}
+
+void BatteryManager::process(){
+
+}
+
+float BatteryManager::getBatteryVoltage(){
+
+}
+
+float BatteryManager::getBatteryLevel(){
+
+}
+
+float BatteryManager::getCellVoltage(uint8_t){
+
+}
+
 BatteryManager::BatteryManager() {
 	// TODO Auto-generated constructor stub
 
@@ -16,3 +36,15 @@ BatteryManager::~BatteryManager() {
 	// TODO Auto-generated destructor stub
 }
 
+
+void BatteryManager::updateMeasurments(){
+
+}
+
+void BatteryManager::checkBatteryLevel(){
+
+}
+
+void BatteryManager::add(LipoCell){
+
+}
