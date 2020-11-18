18dffeb200e94a8c03d93434308368640f0412fe
igorpieniek <pieniek97@gmail.com>
2020-11-18 12:51:44 +0100

    Add class files

---
+++ Core/Inc/SectionSwitch.cpp
@@ -0,0 +1,18 @@
+/*
+ * SectionSwitch.cpp
+ *
+ *  Created on: 18.11.2020
+ *      Author: Igor
+ */
+
+#include <SectionSwitch.h>
+
+SectionSwitch::SectionSwitch() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SectionSwitch::~SectionSwitch() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Core/Inc/SectionSwitch.h
@@ -0,0 +1,17 @@
+/*
+ * SectionSwitch.h
+ *
+ *  Created on: 18.11.2020
+ *      Author: Igor
+ */
+
+#ifndef INC_SECTIONSWITCH_H_
+#define INC_SECTIONSWITCH_H_
+
+class SectionSwitch {
+public:
+	SectionSwitch();
+	virtual ~SectionSwitch();
+};
+
+#endif /* INC_SECTIONSWITCH_H_ */
