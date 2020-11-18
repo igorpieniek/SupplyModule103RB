cf8247bce8ea503f5d801af8858600b0a5f60e09
igorpieniek <pieniek97@gmail.com>
2020-11-18 21:39:43 +0100

    Add energu manager class files

---
+++ Core/Inc/EnergyManager.h
@@ -0,0 +1,17 @@
+/*
+ * EnergyManager.h
+ *
+ *  Created on: 18.11.2020
+ *      Author: Igor
+ */
+
+#ifndef INC_ENERGYMANAGER_H_
+#define INC_ENERGYMANAGER_H_
+
+class EnergyManager {
+public:
+	EnergyManager();
+	virtual ~EnergyManager();
+};
+
+#endif /* INC_ENERGYMANAGER_H_ */
---
+++ Core/Src/EnergyManager.cpp
@@ -0,0 +1,18 @@
+/*
+ * EnergyManager.cpp
+ *
+ *  Created on: 18.11.2020
+ *      Author: Igor
+ */
+
+#include <EnergyManager.h>
+
+EnergyManager::EnergyManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EnergyManager::~EnergyManager() {
+	// TODO Auto-generated destructor stub
+}
+
