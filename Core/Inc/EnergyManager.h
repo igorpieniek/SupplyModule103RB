3f8ccf35884bc36755b84b3e24beba772522e962
igorpieniek <pieniek97@gmail.com>
2020-11-18 21:59:58 +0100

    Add index enums & init definition

--- Core/Inc/EnergyManager.h
+++ Core/Inc/EnergyManager.h
@@ -8,8 +8,50 @@
 #ifndef INC_ENERGYMANAGER_H_
 #define INC_ENERGYMANAGER_H_
 
+
+#include "HallSensor.h"
+#include "SupplyBranch.h"
+#include "SectionSwitch.h"
+
+#define EM_DMA_NUMBER_OF_CONVERSION  9 //5 HALL + 4 VOLTAGE MEASURMENTS (+ 1 internal temperature?)
+#define HALL_SENSOR_NUMBER 5
+#define SUPPLY_BRANCH_NUMBER 4
+#define SECTION_SWITCH_NUMBER 4
+
+
 class EnergyManager {
+private:
+
+	HallSensor* hall_sensors[HALL_SENSOR_NUMBER];
+	SupplyBranch* supply_branches[SUPPLY_BRANCH_NUMBER];
+	SectionSwitch* section_switches[SECTION_SWITCH_NUMBER];
+	enum hall_index{
+		motor,
+		hall_5_1,
+		hall_5_2,
+		hall_3,
+		hall_7
+	};
+	enum supplybranch_index{
+		supply_5_1,
+		supply_5_2,
+		supply_3,
+		supply_7
+	};
+	enum switch_index{
+		switch_5_1,
+		switch_5_2,
+		switch_3,
+		switch_7
+	};
+
+	void hallSensor_init();
+	void supplyBranch_init();
+	void sectionSwitch_init();
 public:
+
+	void init();
+
 	EnergyManager();
 	virtual ~EnergyManager();
 };
