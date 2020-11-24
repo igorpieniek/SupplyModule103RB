6351f7d05d8852459eec5d0b9229b03c57d34cf4
igorpieniek <pieniek97@gmail.com>
2020-11-24 19:51:17 +0100

    Move Fun.cpp file to Src, add doc info

--- Core/Inc/Fun.h
+++ Core/Inc/Fun.h
@@ -11,30 +11,49 @@
 #include "main.h"
 
 
-
+/** Class to define functions typical for Fun */
 class Fun {
 public:
+	/** Velocity levels available to set */
 	enum Velocity{
-		ZERO,
-		LOW = 300,
-		NORMAL = 650,
-		HIGH= 1000
+		ZERO,			/**< Zero speed. Fun if turn OFF*/
+		LOW = 300, 		/**< Low speed : 30% */
+		NORMAL = 650,	/**< Normal speed : 65% */
+		HIGH= 1000		/**< Max speed : 100% */
 	};
 
+	/** Turn ON Fun
+	 * @param vel - Velocity (optional) - normally velocity is set to NORMAL() */
 	void on(Velocity vel= NORMAL);
+
+	/**Turn ON Fun - with custom speed in range <ZERO(), HIGH()>
+	 * @param vel - uint16_t - custom value of Fun speed*/
 	void on(uint16_t vel);
+
+	/**Turn off Fun */
 	void off();
+
+	/** Return info about current state of Fun*/
 	uint8_t isOn()const;
+
+	/** Return current PWM value in percents [%]*/
 	float getPWMvalue();
 
+	/**
+	 * @param tim - TIM_HandleTypeDef * - to define timer to use
+	 * @param ch - uint32_t - timer channel
+	 */
 	Fun(TIM_HandleTypeDef * tim, uint32_t ch);
 	virtual ~Fun();
 private:
 
 	uint8_t is_on;
 	TIM_HandleTypeDef * timer;
+
 	uint32_t channel;
 	uint16_t velocity;
+
+
 
 };
 
