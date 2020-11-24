a7d2950e0ae15eee8cf99f1ee15f0b812b10bd97
igorpieniek <pieniek97@gmail.com>
2020-11-24 14:04:23 +0100

    Create intern temperature class

---
+++ Core/Inc/InternTemperature.h
@@ -0,0 +1,17 @@
+/*
+ * InternTemperature.h
+ *
+ *  Created on: 24.11.2020
+ *      Author: Igor
+ */
+
+#ifndef INC_INTERNTEMPERATURE_H_
+#define INC_INTERNTEMPERATURE_H_
+
+class InternTemperature {
+public:
+	InternTemperature();
+	virtual ~InternTemperature();
+};
+
+#endif /* INC_INTERNTEMPERATURE_H_ */
---
+++ Core/Src/InternTemperature.cpp
@@ -0,0 +1,18 @@
+/*
+ * InternTemperature.cpp
+ *
+ *  Created on: 24.11.2020
+ *      Author: Igor
+ */
+
+#include <InternTemperature.h>
+
+InternTemperature::InternTemperature() {
+	// TODO Auto-generated constructor stub
+
+}
+
+InternTemperature::~InternTemperature() {
+	// TODO Auto-generated destructor stub
+}
+
