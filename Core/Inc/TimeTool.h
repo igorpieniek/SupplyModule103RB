1f7be71929a58a04d7956cccdc49aa94db5402a9
igorpieniek <pieniek97@gmail.com>
2020-12-02 19:45:10 +0100

    Update TimeTool doc

--- Core/Inc/TimeTool.h
+++ Core/Inc/TimeTool.h
@@ -9,15 +9,31 @@
 
 #define TIMETOOL_TIMER htim4
 
+/** Tool to measure time in miliseconds.
+ * It provide function to get current CNT register state and
+ * to compare between many measurments - compareMicros().
+ * It use timer #TIMETOOL_TIMER
+ * @warning #1 Only one global object could be created, but no more!
+ * @warning #2 MX_TIMx_Init() have to be called before init() method
+ */
 extern class TimeTool {
 
 	uint8_t microsInitDone;
 	const uint16_t uint16Half= UINT16_MAX / 2;
 public:
-
+	/**Init function - it does't use MX_TIMx_Init() function - it have to be called before
+	 */
 	void init(void);
+
+	/** Check if object timer is working*/
 	uint8_t isToolInit(void);
+
+	/** @return current state of CNT register*/
 	uint16_t getMicros(void);
+
+	/** To compare two timer states and return the difference.
+	 * @return value in range <0, #UINT16_MAX/2 >
+	 */
 	uint16_t compareMicros(uint16_t a, uint16_t b);
 
 	TimeTool();
