e077405fd922f5a38e60d32c14838759a7e02831
igorpieniek <pieniek97@gmail.com>
2021-03-30 19:41:21 +0200

    AllTasks cleanup

--- Software/Src/AllTasks.cpp
+++ Software/Src/AllTasks.cpp
@@ -20,15 +20,6 @@ osThreadId LedUpHandle;
 void StartBatteryManagerTask(void const * argument);
 void StartLedUpTask(void const * argument);
 
-uint32_t receiveADC[2];
-float per, volt, perAvrg, voltAvrg ;
-uint16_t  currentTime[3]= {0};
-uint8_t RXdata;
-LedNotifier led(LD2_GPIO_Port, LD2_Pin);
-LedNotifier led1(LED1_GPIO_Port, LED1_Pin);
-LedNotifier led2(LED2_GPIO_Port, LED2_Pin);
-
-
 
 void AllTasks_init(){
 
@@ -46,31 +37,7 @@ void AllTasks_init(){
 
 void StartBatteryManagerTask(void const * argument){
 
-
-	uint8_t blinkFlag=0;
-	LipoCell lipo(3.3f);
-	HAL_ADC_Start(&hadc1);
-	HAL_ADC_Start_DMA(&hadc1, receiveADC, 2);
-
 	for(;;){
-
-		lipo.update(receiveADC[0]);
-		per = lipo.getPercentage();
-		volt= lipo.getVoltage();
-		perAvrg = lipo.getPercentageAvrg();
-		voltAvrg= lipo.getVoltageAvrg();
-
-		if(voltAvrg >=2.5f && blinkFlag==0){
-			led.blink(500,1000);
-			led1.blink(1000,200);
-			led2.blink(25);
-			blinkFlag = 1;
-		}
-//		if (voltAvrg< 2.5f){
-//			blinkFlag = 0;
-//			led.on();
-//		}
-
 
 		osDelay(100);
 
@@ -80,50 +47,22 @@ void StartBatteryManagerTask(void const * argument){
 
 void StartLedUpTask(void const * argument){
 
-	time_tool.init();
 
 	for(;;){
 
-
-		currentTime[2]= time_tool.getMicros();
-		currentTime[0]= time_tool.compareMicros(currentTime[2], currentTime[1]);
-		currentTime[1] = currentTime[2];
-
-		led.blink_process();
-		led1.blink_process();
-		led2.blink_process();
-//		osEvent evt = osSignalWait(0x01 | 0x02 | 0x04| 0x08,osWaitForever);
-//			if      (evt.value.signals == 0x01){
-//				led.on();
-//
-//			}
-//			else if (evt.value.signals == 0x02){
-//				led.off();
-//
-//			}
-//			else if (evt.value.signals == 0x04){
-//				led.blink_config(100);
-//
-//			}
-//			else if (evt.value.signals== 0x08){
-//				led.blink_process();
-//
-//			}
-
-
 		osDelay(10);
-
 	}
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+
 	//static uint16_t per =500;
 //	if	   (RXdata == 'n') led.on();
 //	else if(RXdata == 'f') led.off();
 //	else if(RXdata == 'b') led.blink_config(500,1000);
 
 
-	HAL_UART_Receive_IT(&huart3, &RXdata, 1);
+//	HAL_UART_Receive_IT(&huart3, &RXdata, 1);
 }
 
 
