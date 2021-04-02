eb840e66d8a5ff1c5755feda716a9cd594b452e7
igorpieniek <pieniek97@gmail.com>
2021-04-02 19:46:41 +0200

    ErrorHandler: prepare all definitions
    - add methods to check if buffer is empty
    - method to get last error msg

--- Software/Inc/ErrorHandler.h
+++ Software/Inc/ErrorHandler.h
@@ -15,18 +15,29 @@
 #define THROW_ERROR()       er.insert(__LINE__, __FILE__ );
 #define THROW_ERROR_M(msg)  er.insert(__LINE__, __FILE__, msg);
 
+#define ERROR_BUFFER_SIZE 10
 class ErrorHandler {
 public:
 	void insert(int line, std::string file, std::string msg = {} );
+	uint8_t isBufferEmpty();
+
+	std::string getLastError();
+
 	ErrorHandler();
 	virtual ~ErrorHandler();
 
 private:
-	std::string buffer[10];
+	std::string buffer[ERROR_BUFFER_SIZE];
 	uint8_t errorCounter;
 	const uint8_t fileLetters = 4;
 	std::string convertToErrorMsg(int line, std::string file, std::string msg );
 	void addToBuffer(std::string msg);
+	void incrementBufferIndex();
+	void decrementBufferIndex();
+
+	void deleteLastError();
+
+	char getFileExtension(std::string file);
 
 };
 
--- Software/Src/ErrorHandler.cpp
+++ Software/Src/ErrorHandler.cpp
@@ -7,9 +7,11 @@
 
 #include <ErrorHandler.h>
 
+
+ErrorHandler errorHandler;
+
 ErrorHandler::ErrorHandler() {
 	errorCounter = 0;
-
 }
 
 ErrorHandler::~ErrorHandler() {
@@ -18,16 +20,50 @@ ErrorHandler::~ErrorHandler() {
 
 void ErrorHandler::insert(int line, std::string file, std::string msg) {
 	std::string fullMsg = convertToErrorMsg(line, file, msg);
-
-
-
+	addToBuffer(fullMsg);
 }
 
 std::string ErrorHandler::convertToErrorMsg(int line, std::string file, std::string msg) {
-	return file[fileLetters] + std::to_string(line) + msg;
+	char fileExt = getFileExtension(file);
+	return file.substr(0, fileLetters) +fileExt + std::to_string(line) + msg;
+}
+
+char ErrorHandler::getFileExtension(std::string file){
+	if(file.find(".c") != std::string::npos) return 'c';
+	else 									 return 'h';
 }
 
 void ErrorHandler::addToBuffer(std::string msg){
 	buffer[errorCounter] = msg;
-	errorCounter++;
+	incrementBufferIndex();
+}
+
+void ErrorHandler::incrementBufferIndex(){
+	if (errorCounter < ERROR_BUFFER_SIZE -1){
+		errorCounter++;
+	}
+}
+
+void ErrorHandler::decrementBufferIndex(){
+	if(errorCounter > 0){
+		errorCounter--;
+	}
+}
+
+std::string ErrorHandler::getLastError(){
+	std::string last = buffer[errorCounter];
+	deleteLastError();
+	return last;
+}
+
+
+
+uint8_t ErrorHandler::isBufferEmpty(){
+	if(errorCounter == 0) return 1;
+	else 				  return 0;
+}
+
+void ErrorHandler::deleteLastError(){
+	buffer[errorCounter] = "";
+	decrementBufferIndex();
 }
