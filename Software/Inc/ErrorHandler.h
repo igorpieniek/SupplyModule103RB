3809a1f215b24d273d280c9e4aa058ad07cab5ff
igorpieniek <pieniek97@gmail.com>
2021-04-02 17:31:04 +0200

    ErrorHandler: add new clas files

---
+++ Software/Inc/ErrorHandler.h
@@ -0,0 +1,17 @@
+/*
+ * ErrorHandler.h
+ *
+ *  Created on: 2 kwi 2021
+ *      Author: Igor
+ */
+
+#ifndef INC_ERRORHANDLER_H_
+#define INC_ERRORHANDLER_H_
+
+class ErrorHandler {
+public:
+	ErrorHandler();
+	virtual ~ErrorHandler();
+};
+
+#endif /* INC_ERRORHANDLER_H_ */
---
+++ Software/Src/ErrorHandler.cpp
@@ -0,0 +1,18 @@
+/*
+ * ErrorHandler.cpp
+ *
+ *  Created on: 2 kwi 2021
+ *      Author: Igor
+ */
+
+#include <ErrorHandler.h>
+
+ErrorHandler::ErrorHandler() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ErrorHandler::~ErrorHandler() {
+	// TODO Auto-generated destructor stub
+}
+
